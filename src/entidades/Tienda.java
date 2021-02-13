687b678ab9fd9123ca375fa9bffaa5252cded091
maria23051982 <77923900+maria23051982@users.noreply.github.com>
2021-02-13 16:22:29 -0300

    Cambios en el formato de la tienda.

--- src/entidades/Tienda.java
+++ src/entidades/Tienda.java
@@ -1,12 +1,15 @@
 package entidades;
 
 public class Tienda {
-	private String Dueño;
-	private String Direccion;
-	private String Telefono;
-		
+	String Tienda = "Tienda Fotografica grupo 1";
+	String Direccion = "Avenida De Los Trabajadores 4500"+ " Mar del Plata";
+	String Telefono = "2234698800";
 	
 	
+	System.out.println(Tienda );
+	System.out.println(Direccion);
+	System.out.println(Telefono);
+    System.out.println("**************************************************");
 	
 	
 }
