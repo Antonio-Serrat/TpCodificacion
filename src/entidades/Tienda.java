298b810a58247e6e846cbfd1e5bac1b198a80b6b
Tonisrt <4antoniogabriel20@gmail.com>
2021-02-13 16:29:13 -0300

    Creados geters and seters/ cambiose dueño por duenio

--- src/entidades/Tienda.java
+++ src/entidades/Tienda.java
@@ -1,9 +1,29 @@
 package entidades;
 
 public class Tienda {
-	private String Dueño;
+	private String Duenio;
 	private String Direccion;
 	private String Telefono;
+	
+	
+	public String getDuenio() {
+		return Duenio;
+	}
+	public void setDuenio(String duenio) {
+		Duenio = duenio;
+	}
+	public String getDireccion() {
+		return Direccion;
+	}
+	public void setDireccion(String direccion) {
+		Direccion = direccion;
+	}
+	public String getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(String telefono) {
+		Telefono = telefono;
+	}
 		
 	
 	
