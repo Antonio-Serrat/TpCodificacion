7a8c55abc2ed7018dd5e565c25682358c0ef556e
maria23051982 <77923900+maria23051982@users.noreply.github.com>
2021-02-13 16:24:11 -0300

    Prueba ingreso datos clientes.

---
+++ src/entidades/Cliente.java
@@ -0,0 +1,24 @@
+package entidades;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		String identificacion;
+		String cliente;
+		String direccion;
+		String telefono;
+		
+	    identificacion = "0001";
+	    cliente = "Juan Perez";
+		direccion = "12 de octubre 3688 "+ " Mar del Plata";
+		telefono = "0223- 489-1544";
+		
+		System.out.println(identificacion);
+		System.out.println(cliente);
+		System.out.println(direccion);
+		System.out.println(telefono);
+        System.out.println("**************************************************");
+
+	}
+
+}
