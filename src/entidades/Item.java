847b6fa591e4c78ba234f6928d607d4d6b991cb7
ailenamodio <ailenamodio@gmail.com>
2021-02-13 23:26:28 -0300

    prueba 2

--- src/entidades/Item.java
+++ src/entidades/Item.java
@@ -1,5 +1,18 @@
 package entidades;
 
+import java.util.Date;
+
 public class Item {
 	
-}
+	private int codReferencia;
+	private String estado;
+	private Double costoAlquiler;
+	private Date fechaDevolucion = new Date();
+	//historialAlquiler
+	
+	public void cambiarEstado (Estado estado) {
+		
+	}
+		
+	}
+
