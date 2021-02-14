105b654ca66532bde7ff4402aed8099371887d84
PC <PC@192.168.0.80>
2021-02-13 21:32:02 -0300

    Estado Alquilada, ConRetraso, Disponible, EnReparacion

---
+++ src/entidades/EnReparacion.java
@@ -0,0 +1,15 @@
+package entidades;
+
+public class EnReparacion extends Estado {
+	
+	public EnReparacion () {
+		this.nombre= "EnReparacion";
+	}
+
+	public Estado getEstadoSiguiente() {
+		// TODO Auto-generated method stub
+		return this;
+
+	}
+
+}
