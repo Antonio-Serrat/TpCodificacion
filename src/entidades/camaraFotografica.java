7233397a83cb35760411bf44a0c6e547ca1ff998
ailenamodio <ailenamodio@gmail.com>
2021-02-15 01:58:26 -0300

    Se creo la clase camaraFotografica

--- src/entidades/camaraFotografica.java
+++ src/entidades/camaraFotografica.java
@@ -1,5 +1,27 @@
 package entidades;
 
 public class camaraFotografica {
-
+	private String marca;
+	private String modelo;
+	private boolean soporteFlash=true;
+	public String getMarca() {
+		return marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public boolean isSoporteFlash() {
+		return soporteFlash;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public void setSoporteFlash(boolean soporteFlash) {
+		this.soporteFlash = soporteFlash;
+	} 
+	
+	
 }
