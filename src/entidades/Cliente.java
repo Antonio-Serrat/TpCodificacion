bde1e568d97b6bfc32f2ce6ea75933a3e90dd62c
vieradaniel <dariennx@hotmail.com>
2021-02-22 17:30:11 -0300

    agregue una lista como atributo para alojar camaras alquiladas ,sus metodos de agregar y quitar camaras y sus getters y setters

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -1,21 +1,44 @@
 package entidades;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cliente {
 	private String nombre;
 	private String direccion;
 	private int telefono;
-	//private List<Alquiler>historialAlquiler;
+	private List<Item>camarasAlquiladas;
 	private int mesesPenalizacion;
 	
 	
 	public Cliente(String nombre) {
 		super();
 		this.nombre = nombre;
+		this.camarasAlquiladas = new ArrayList<>();
+	}
+
+	
+	public List<Item> getCamarasAlquiladas() {
+		return camarasAlquiladas;
 	}
 
 
+
+
+	public void setCamarasAlquiladas(List<Item> camarasAlquiladas) {
+		this.camarasAlquiladas = camarasAlquiladas;
+	}
+
+
+
+
+	public void agregarCamarasAlquiladas(Item item) {
+		this.camarasAlquiladas.add(item);
+	}
+	public void quitarCamarasAlquiladas(Item item) {
+		this.camarasAlquiladas.remove(item);
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
